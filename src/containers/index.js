33b3f5702d5db3b85059ac03c0021f5c6bb8c81d
pritam-patil <pritam18121990@gmail.com>
2019-03-27 19:59:19 +0530

    Error reporting with AirBrake
    Changes:
    - added ErrorBoundary core component
    - wrapped Home component in ErrorBoundary component

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { object } from 'prop-types';
 import { Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import ErrorReporter from '../components/core';
 import { BottomNav, Header } from '../components';
 import '../index.scss';
 
@@ -17,13 +18,15 @@ const HomeScreen = props => {
   const { classes } = props;
 
   return (
-    <div className="app">
-      <Header />
-      <Typography variant="body2" color="inherit" className={classes.content}>
-        Content Here!
-      </Typography>
-      <BottomNav />
-    </div>
+    <ErrorReporter>
+      <div className="app">
+        <Header />
+        <Typography variant="body2" color="inherit" className={classes.content}>
+          Content Here!
+        </Typography>
+        <BottomNav />
+      </div>
+    </ErrorReporter>
   );
 };
 
